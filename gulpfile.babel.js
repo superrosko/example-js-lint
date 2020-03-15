b607a68d9ff52c6101c25e0a206ccaa686dc7071
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-03-15 22:53:49 +0300

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+/**
+ * Импортируем необходимые пакеты
+ */
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+
+/**
+ * Указываем пути к файлам для проверки
+ * @type {{js: string}}
+ */
+const paths = {
+  js: './src/**/*.js',
+};
+
+/**
+ * Test JS lint
+ * @return {*}
+ */
+function testJsLint() {
+  return gulp.src(paths.js).
+      pipe(eslint()).
+      pipe(eslint.format()).
+      pipe(eslint.failAfterError());
+}
+
+/**
+ * Экспортируем методы
+ */
+const tests = gulp.parallel(testJsLint);
+exports.tests = tests;
+
+export default tests;
